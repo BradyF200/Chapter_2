156bda1b51d9aa938e5ca5881f1dfd1bfb325d30
BradyF200 <Brady.faux@fwcsstudents.org>
2024-09-05 13:19:00 -0400

    Add files via upload

---
+++ Faux_TIY_2_11.py
@@ -0,0 +1,17 @@
+# Whats My Favorite Number
+Favorite_Num=22
+
+
+
+Message=f'Your Favorite number is {Favorite_Num}.'
+print(Message)
+
+
+# Add up to 8
+print(5+3)
+
+print(2*4)
+
+print(12-4)
+
+print(16/2)
