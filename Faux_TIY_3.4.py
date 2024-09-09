dd0b08af4a06d49ecc27ee2fddf985e6792e0693
BradyF200 <Brady.faux@fwcsstudents.org>
2024-09-09 12:41:28 -0400

    Add files via upload

---
+++ Faux_TIY_3.4.py
@@ -0,0 +1,9 @@
+invite=['keanu reeves','post malone','john cena']
+
+
+invite_1=f"Dear {invite[0].title()}. I would like to invite you to a diner party"
+print(invite_1)
+invite2=f'Dear {invite[1].title()}. I would like you to join Our dinner party'
+print(invite2)
+invite3=f'Dear {invite[2].title()}. Please join the party'
+print(invite3)
