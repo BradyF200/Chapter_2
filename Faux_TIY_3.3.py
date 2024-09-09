8fb2dadbf2cae936c0c8b7444cb5a931b4e58a43
BradyF200 <Brady.faux@fwcsstudents.org>
2024-09-09 12:06:33 -0400

    Add files via upload

---
+++ Faux_TIY_3.3.py
@@ -0,0 +1,6 @@
+car=['camaro','hell cat', 'aston martin', 'Motorcycle' ]
+message=f"I like the {car[2].title()}. from James Bond"
+print(message)
+
+Message=f"i would love to own a fast {car[3].title()}."
+print(Message)
