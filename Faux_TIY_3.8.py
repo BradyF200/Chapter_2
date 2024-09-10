2af902bc80a8c9b8e7b56189aaa9e0349c7b0b19
BradyF200 <Brady.faux@fwcsstudents.org>
2024-09-10 13:29:44 -0400

    Add files via upload

---
+++ Faux_TIY_3.8.py
@@ -0,0 +1,34 @@
+# List
+place=['New Your', 'Chicago', ' Wyoming', 'Space', 'Seattle']
+print(place)
+# Sort List
+place.sort()
+print(place)
+# Revers Order
+place.sort(reverse=True)
+print(place)
+# Original order vs the sorted
+place=['New Your', 'Chicago', ' Wyoming', 'Space', 'Seattle']
+print(' This is the original order')
+print(place)
+
+print('This is the sorted list')
+print(sorted(place))
+
+print('Here is the original order again')
+print(place)
+
+# Print Reverse
+place.reverse()
+print(place)
+
+place.reverse()
+print(place)
+
+place.sort()
+print(place)
+
+place.sort(reverse=True)
+print(place)
+
+
