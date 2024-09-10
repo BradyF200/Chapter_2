15960d7a3803758477bcfb8dd62488ce98513b22
BradyF200 <Brady.faux@fwcsstudents.org>
2024-09-10 13:47:05 -0400

    Add files via upload

---
+++ Faux_TIY_3.9.py
@@ -0,0 +1,16 @@
+#Invatations
+people=['keanu reeves','post malon','john cena']
+Count_people=len(people)
+
+print("{} This is who i would like to invite to  my party".format(Count_people))
+
+
+
+
+
+
+
+
+
+
+
