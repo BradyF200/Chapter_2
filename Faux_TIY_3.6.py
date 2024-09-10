cd51483f5ce21b6696308250bd402a88e68c9cb2
BradyF200 <Brady.faux@fwcsstudents.org>
2024-09-10 12:25:13 -0400

    Add files via upload

---
+++ Faux_TIY_3.6.py
@@ -0,0 +1,67 @@
+#Invatations
+people=['keanu reeves','post malon','john cena']
+
+J=people[0].title()
+print(J + " im hosting a dinner party i would like you to attend")
+
+J=people[1].title()
+print(J + ' please join the party')
+
+J=people[2].title()
+print(J + " you are welcome to join the party")
+
+J=people[1].title()
+print(J + ' will not make it to  the party')
+
+# A guest is sick
+del(people[1])
+people.insert(1, " Seth Freaking Rollins")
+
+J=people[0].title()
+print(J + " im hosting a dinner party i would like you to attend")
+
+J=people[1].title()
+print(J + ' please join the party')
+
+J=people[2].title()
+print(J + " you are welcome to join the party")
+
+
+# More room
+print('we got a bigger tabel')
+
+people.insert(0, 'donald trump')
+people.insert(2, 'Kendrick')
+people.append('Jason')
+
+J=people[0].title()
+print(J + ' pary invite')
+
+J=people[1].title()
+print(J + " pary invite")
+
+J=people[2].title()
+print( J + " party invite")
+
+J=people[3].title()
+print(J + " party invite ")
+
+J=people[4].title()
+print(J + 'pary invite')
+
+J=people[5].title()
+print(J + ' pary invite')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
