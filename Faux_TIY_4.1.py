4048e68d5dd1cfa2f56191cb018203489204ea86
BradyF200 <Brady.faux@fwcsstudents.org>
2024-09-12 13:32:04 -0400

    Add files via upload

---
+++ Faux_TIY_4.1.py
@@ -0,0 +1,7 @@
+pizza=["Cheese", "pepperoni", " Sausage"]
+
+# Names Of Pizzas
+for favorite in pizza:
+    print(f'these are my favorite {favorite} pizzas')
+
+print("\nI really i like pizza a lot")
