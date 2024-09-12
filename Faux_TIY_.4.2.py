5fcbc5a7699fec4dc30377626cac04d79565e315
BradyF200 <Brady.faux@fwcsstudents.org>
2024-09-12 13:46:09 -0400

    Add files via upload

---
+++ Faux_TIY_.4.2.py
@@ -0,0 +1,12 @@
+Animal=['cat', 'dog' , 'horse']
+print(Animal)
+
+for farm_animal in Animal:
+    print(Animal)
+
+print("\n")
+
+for farm_animal in Animal:
+    print(f'{farm_animal} are what most farmers prefer')
+
+print("\nAll of the animals are good farm animals")
